e487e9b1d930cec8efec1ae2514b5f5b22c47373
mmmarciii <mesz.marc@gmail.com>
2025-12-27 12:24:39 +0100

    Add README

---
+++ README.md
@@ -0,0 +1,13 @@
+## 🚀 Functions
+
+- **CRUD operations:** View, add, delete, and update contact records...
+- **Interactive menu:** User-friendly menu system navigable with arrow keys.
+- **Phone Number validation:** Validation:(`libphonenumber-csharp`).
+- **Databes:** SQLite/SQL Server integration via Entity Framework Core.
+
+## 🛠️ Tech stack
+
+- **Language:** C# (.NET 10)
+- **ORM:** Entity Framework Core
+- **UI:** [Spectre.Console](https://spectreconsole.net/)
+- **Validation:** Libphonenumber-csharp
--- obj/Debug/net10.0/PhoneBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/PhoneBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhoneBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+031341d7f2b5ab7137e4e349f468abaed23d7ede")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhoneBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhoneBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
